86af2056c3c1a47a1c429447a8d24de301bd5247
Asus <you@example.com>
2020-05-20 13:46:19 +0530

    this is my first commit

---
+++ app/src/main/java/com/example/weatherhere/base/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.weatherhere.base
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weatherhere.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
